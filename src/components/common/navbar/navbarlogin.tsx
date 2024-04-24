50b83bd317acdd1823d9713e7d0c38cab19c6f93
Ayushkotadiya1 <ayushkotadiya351@gmail.com>
2024-04-24 18:28:55 +0530

    some validation add

--- src/components/common/navbar/navbarlogin.tsx
+++ src/components/common/navbar/navbarlogin.tsx
@@ -58,7 +58,6 @@ const NavbarLogin = () => {
 
                     </div>
                     <div className="flex gap-3">
-                        <Link href={'/createaccount'}>Create account</Link>
                         <button onClick={() => setIsSignIn(!isSignIn)} className='hidden md:flex px-6 py-[10px] rounded-lg font-Poppins font-medium text-[18px] leading-[18px] text-primary border border-primary'>Sign Up</button>
                         <button onClick={() => setIsSignIn(!isSignIn)} className='px-[34px] py-[10px] rounded-lg font-Poppins font-medium text-[18px] leading-[18px] text-white bg-[#F13170]'>Log In</button>
                     </div>
